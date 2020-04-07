b123a6faa2a5dc259f90762581892e4038aa87ad
Allan Farca <allan.farca@gmail.com>
2020-04-07 00:33:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog 
+  
+  def name= (name)
+    @dogs_name = "#{name}"
+  end
+  
+  def name 
+    @dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+  
 end
 
+fido = Dog.new
+fido.name = "Fido"
+ 
+fido.name
+ 
+fido.bark
